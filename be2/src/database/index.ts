190ce94a9251b770d88316b1f9be1910493a536f
Gylmynnn <muhamadgilman999@gmail.com>
2025-08-30 00:55:32 +0800

    core : connect drizzle client

---
+++ be2/src/database/index.ts
@@ -0,0 +1,15 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+// import { createClient } from '@supabase/supabase-js'
+import postgres from "postgres";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const client = postgres(process.env.DATABASE_URL!);
+const db = drizzle(client);
+
+// const supabase = createClient(
+//     process.env.SUPABASE_URL!,
+//     process.env.SUPABASE_SERVICE_ROLE_KEY!
+// )
+export { db }
+
