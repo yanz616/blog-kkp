fc8af3170ae616c39aee2baf47883764c98b2aac
Gylmynnn <muhamadgilman999@gmail.com>
2025-09-01 22:57:03 +0800

    feat : userpaload interface

--- be2/src/lib/dto/auth.ts
+++ be2/src/lib/dto/auth.ts
@@ -1,8 +1,15 @@
+interface UserPayloadData {
+    userId: number;
+    username: string;
+    email: string;
+}
+
 interface LoginResponse {
     id: number;
     username: string;
     email: string;
     token: string;
+    avatar: string;
     createdAt: string;
     updatedAt: string;
 }
@@ -12,7 +19,19 @@ interface LoginRequest {
     password: string;
 }
 
+interface RegisterResponse {
+    id: number;
+    username: string;
+    email: string;
+    avatar?: string;
+    createdAt: string;
+}
+
+interface RegisterRequest {
+    username: string;
+    email: string;
+    password: string;
+}
 
 
-
-export { LoginResponse, LoginRequest }
+export { LoginResponse, LoginRequest, RegisterResponse, RegisterRequest, UserPayloadData }
