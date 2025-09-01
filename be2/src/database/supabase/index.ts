125e363c84f839483792000f8f9142d5ec738011
Gylmynnn <muhamadgilman999@gmail.com>
2025-09-01 22:55:50 +0800

    core : supabase func

---
+++ be2/src/database/supabase/index.ts
@@ -0,0 +1,32 @@
+import sharp from 'sharp'
+import { v4 as uuidv4 } from 'uuid'
+import { supabase } from '../index.js';
+import { UploadResponse } from './type.js';
+
+
+export async function SupabaseDelete(assetId: string): Promise<void> {
+    const { error } = await supabase.storage.from("storage").remove([assetId]);
+    if (error) throw error;
+}
+
+export async function SupabaseUpload(buffer: Buffer, folder: string): Promise<UploadResponse> {
+    const uniqueName = `${folder}/${uuidv4()}.webp`;
+    const webpBuffer = await sharp(buffer)
+        .resize(1080)
+        .webp({ quality: 80 })
+        .toBuffer()
+
+    const { error } = await supabase.storage
+        .from('storage')
+        .upload(uniqueName, webpBuffer, {
+            contentType: 'image/webp',
+            upsert: false,
+        })
+
+    if (error) throw error
+    const { data } = supabase.storage.from('storage').getPublicUrl(uniqueName)
+    return {
+        assetId: uniqueName,
+        url: data.publicUrl,
+    }
+}
