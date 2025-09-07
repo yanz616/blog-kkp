b847acff09ac980cc57724b8e382afb6c224d4a6
Adriyan <adriyanmazkur616@gmail.com>
2025-09-07 11:55:59 +0800

    update: posr, myCard, cardPub

--- fe/lib/presentation/blocs/posts/post_bloc.dart
+++ fe/lib/presentation/blocs/posts/post_bloc.dart
@@ -8,16 +8,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class PostBloc extends Bloc<PostEvent, PostState> {
   final PostRepository postRepository;
 
-  PostBloc(this.postRepository) : super(PostsInitial()) {
+  PostBloc(this.postRepository) : super(const PostsInitial()) {
     on<CreatePost>((event, emit) async {
-      emit(PostsLoading());
+      emit(const PostsLoading());
 
-      final response = await postRepository.createPost(event.postRequest);
+      try {
+        final response = await postRepository.createPost(event.postRequest);
 
-      if (response is SuccessResponse<PostModel>) {
-        emit(PostsSuccess(response.message, response.data));
-      } else if (response is ErrorResponse) {
-        emit(PostsFailure(response.message));
+        if (response is SuccessResponse<PostModel>) {
+          emit(PostsSuccess(response.message, response.data));
+        } else if (response is ErrorResponse) {
+          emit(PostsFailure(response.message));
+        } else {
+          emit(const PostsFailure("Terjadi kesalahan yang tidak diketahui"));
+        }
+      } catch (e) {
+        emit(PostsFailure("Exception: ${e.toString()}"));
       }
     });
   }
